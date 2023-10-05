fb0e28e30bf574fd8205ed0848b5bc41646ed8a2
Isaac Turner <spacey_sooty@outlook.com>
2023-10-05 14:05:46 +0800

    added control flow

---
+++ control_flow/src/main.rs
@@ -0,0 +1,123 @@
+fn main() {
+    // if statements
+
+    let number = 3;
+
+    // error because number is not a bool
+    // if number {
+    //     println!("number is 3");
+    // }
+
+    if number < 5 {
+        println!("Condition is true");
+    } else {
+        println!("Condition is false");
+    }
+
+    if number != 0 {
+        println!("number was something other than zero");
+    }
+
+    // handling multiple cases
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    // using if in a let statement
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {}", number);
+
+    // error because of different types
+    // let condition = true;
+
+    // let number = if condition { 5 } else { "six" };
+
+    println!("The value of number is: {}", number);
+
+    // loops
+
+    // infinite loop
+    // loop {
+    //     println!("Again!");
+    // }
+
+    // returning values from a loop
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+
+    // labelling loops
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {}", count);
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {}", count);
+
+    // conditional loops with while
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    // using for loops to iterate through things
+    // equivalent while loop
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+
+    // much better for loop
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {}", element);
+    }
+
+    // more efficient for loop as well
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
+}
+

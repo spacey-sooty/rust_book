3357a278b5af204449508681550562a50414cfd3
Isaac Turner <spacey_sooty@outlook.com>
2023-10-05 11:45:35 +0800

    data types

---
+++ data_types/src/main.rs
@@ -0,0 +1,84 @@
+fn main() {
+    let guess: u32 = "42".parse().expect("Not a number!");
+    
+    // floating point types
+    let x = 2.0; // f64
+    let y: f32 = 3.0; // f32
+
+    // math
+
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let remainder = 43 % 5;
+
+    // bool
+    let t = true;
+    let f: bool = false; // with explicit type notation
+
+    // char
+    let c = 'z';
+    let z: char = 'ℤ'; // with explicit type annotation
+    let heart_eyed_cat = '😻';
+
+    // tuples
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+    println!("The value of y is: {}", y);
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
+
+    // arrays
+    // implied typing here is [i32; 5]
+    let a = [1, 2, 3, 4, 5];
+
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    // here we initialise a with 5 elements of 3
+    let a = [3; 5];
+
+    // indexing
+    let first = a[0];
+    let second = a[1];
+    
+    fn outside_of_index_example() {
+        use std::io;
+        let a = [1, 2, 3, 4, 5];
+
+        println!("Please enter an array index.");
+
+        let mut index = String::new();
+
+        io::stdin()
+            .read_line(&mut index)
+            .expect("Failed to read line");
+
+        let index: usize = index
+            .trim()
+            .parse()
+            .expect("Index entered was not a number");
+
+        let element = a[index];
+
+        println!("The value of the element at index {index} is: {element}");
+    }
+}
+

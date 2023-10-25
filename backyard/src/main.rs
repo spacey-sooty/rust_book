390fca2c8589aeec720fa4205ebde340f1547117
Isaac Turner <spacey_sooty@outlook.com>
2023-10-25 18:28:28 +0800

    fix build and use cargo workspace

---
+++ backyard/src/main.rs
@@ -0,0 +1,34 @@
+use crate::garden::vegetables::Asparagus;
+use std::collections::HashMap;
+use rand::Rng;
+
+use std::cmp::Ordering;
+
+use std::io::{self, Write};
+
+// wild card
+use std::collections::*;
+
+pub mod garden;
+
+// use std::fmt;
+// use std::io::Result as IoResult;
+
+
+// fn function1() -> fmt::Result {
+//     // --snip--
+// }
+//
+// fn function2() -> IoResult<()> {
+//     // --snip--
+// }
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let plant = Asparagus {};
+    println!("I'm growing {:?}!", plant);
+
+    let mut map = HashMap::new();
+    map.insert(1, 2);
+}
+

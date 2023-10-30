433237145b2da70717f13c418a72d7e4268718e1
Isaac Turner <spacey_sooty@outlook.com>
2023-10-30 09:32:35 +0800

    error handling using types

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,6 +2,24 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
 
@@ -22,6 +40,11 @@ fn main() {
             Ok(num) => num,
             Err(_) => continue,
         };
+
+        if guess < 1 || guess > 100 {
+            println!("Guess should be between 1 and 100");
+            continue
+        }
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),

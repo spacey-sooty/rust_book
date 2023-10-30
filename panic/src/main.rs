df984459789fc8f61fa868da0a88726fd38f3784
Isaac Turner <spacey_sooty@outlook.com>
2023-10-30 09:24:04 +0800

    result

--- panic/src/main.rs
+++ panic/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    std::env::set_var("RUST_BACKTRACE", "full");
+    std::env::set_var("RUST_BACKTRACE", "full"); // normally 1 is fine here
     let v = vec![1, 2, 3];
 
-     v[99];
+    v[99];
 }
